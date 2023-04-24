e371852c143e37430f9e79f2866219fa6d4acca4
CS341W-Admin <88976618+CS341W-Admin@users.noreply.github.com>
2023-04-24 15:18:59 -0700

    Create selectors.js

---
+++ selectors.js
@@ -0,0 +1,8 @@
+// Assign values and EXPORT these HTML selectors by name
+
+const startBtn = null
+const orderForm = null
+const payForm = null
+const orderStatus = null
+const orderHistory = null
+const presentFood = null
